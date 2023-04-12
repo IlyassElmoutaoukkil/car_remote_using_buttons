093f3e6699cd73261329a9560db940d84ecc8a82
IlyassElmoutaoukkil <31111186+IlyassElmoutaoukkil@users.noreply.github.com>
2023-04-12 11:36:35 +0000

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,9 @@
+basic.forever(function () {
+	let down = pins.digitalReadPin(DigitalPin.P0)
+	let up = pins.digitalReadPin(DigitalPin.P3)
+	let right = pins.digitalReadPin(DigitalPin.P1)
+	let left = pins.digitalReadPin(DigitalPin.P2)
+
+
+    
+})
