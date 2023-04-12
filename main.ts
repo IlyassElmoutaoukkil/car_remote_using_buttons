9907d2d1c2a842006802e1f8400941da68ae2173
IlyassElmoutaoukkil <31111186+IlyassElmoutaoukkil@users.noreply.github.com>
2023-04-12 14:14:23 +0000

    working very well

--- main.ts
+++ main.ts
@@ -1,9 +1,34 @@
+let previousClickValue = false
+let click = false
+let left = false
+let right = false
+let up = false
+let down = false
+radio.setGroup(2)
 basic.forever(function () {
-	let down = pins.digitalReadPin(DigitalPin.P0)
-	let up = pins.digitalReadPin(DigitalPin.P3)
-	let right = pins.digitalReadPin(DigitalPin.P1)
-	let left = pins.digitalReadPin(DigitalPin.P2)
-
-
-    
+    down = pins.digitalReadPin(DigitalPin.P2) == 1
+    up = pins.digitalReadPin(DigitalPin.P1) == 1
+    right = pins.digitalReadPin(DigitalPin.P3) == 1
+    left = pins.digitalReadPin(DigitalPin.P0) == 1
+    console.log(down)
+    click = input.buttonIsPressed(Button.A)
+if ((input.buttonIsPressed(Button.A) || input.buttonIsPressed(Button.B)) && previousClickValue != click) {
+        previousClickValue = click
+        radio.sendNumber(0)
+        return
+    }
+    previousClickValue = click
+    if (down) {
+        return radio.sendNumber(2)
+    }
+    if (up) {
+        return radio.sendNumber(1)
+    }
+    if (right) {
+        return radio.sendNumber(4)
+    }
+    if (left) {
+        return radio.sendNumber(3)
+    }
+    return radio.sendNumber(5)
 })
